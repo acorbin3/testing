8b9a00fd4186b20a38f4b2000e179d5a446399e8
Adam Corbin <acorbin3@gmail.com>
2024-08-25 13:16:45 -0400

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Adam Corbin on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
