36baa13fe887dfefbd49d50a68489416e024d8b4
Adam Corbin <acorbin3@gmail.com>
2024-08-25 13:25:40 -0400

    Adding SwiftUIView

---
+++ testing/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  testing
+//
+//  Created by Adam Corbin on 8/25/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
