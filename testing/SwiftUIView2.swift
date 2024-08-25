4a9b67b77e208cab8fa17ea71920baf72f953381
Adam Corbin <acorbin3@gmail.com>
2024-08-25 13:32:32 -0400

    add new file

---
+++ testing/SwiftUIView2.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView2.swift
+//  testing
+//
+//  Created by Adam Corbin on 8/25/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView2()
+}
